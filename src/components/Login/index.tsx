83e84da546c044a14b900bec08ade4885ca6c1fc
Joanne Silva <joanneegabriela@gmail.com>
2023-11-26 15:10:19 -0300

    chore(modal): updating authentication modals

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -51,22 +51,24 @@ const Login = (props: LoginProps) =>{
     return(
         <BackgroundDropdown>
             <div className={style.login}>
+                <div className={style.login__content}>
                 <div className={style.login__title}>
                     <h2 className={style.title}>Welcome back!</h2>
                     <h3 className={style.subtitle}>Your organized world awaits...</h3>
                 </div>
-                <div className={style.login__content}>
                     <button className={style.login__content__button_close} onClick={() => props.setVisibleLogin(false)}><img src="/assets/close.svg" alt="fechar" /></button>
-                    <label htmlFor="email" className={style.login__content__label}>
-                        <p>E-mail Adress</p>
-                        <input type="email" name="email"  placeholder="Enter your best e-mail" onChange={(e) => setLogin(e.target.value)} value={login}/>
-                    </label>
-                    <label htmlFor="password" className={style.login__content__label}>
-                        <p>Password Adress</p>
-                        <input type="password" name="password" placeholder="Enter a strong password" onChange={(e) => setPassword(e.target.value)} value={password} onKeyUp={getEnter}/>
-                    </label>
-                    {status === "error" ? <p className={style.login__content_errorLogin}>Erro no login...</p> : false}
-                    <button className={`${style.login__content__button_login} ${status === "loading" || status === "success" ? style.active: ""}`} onClick={() => mutate()}>Login</button>
+                    <div className={style.login__form}>
+                        <label htmlFor="email" className={style.login__content__label}>
+                            <p>E-mail Adress</p>
+                            <input type="email" name="email"  placeholder="Enter your best e-mail" onChange={(e) => setLogin(e.target.value)} value={login}/>
+                        </label>
+                        <label htmlFor="password" className={style.login__content__label}>
+                            <p>Password Adress</p>
+                            <input type="password" name="password" placeholder="Enter a strong password" onChange={(e) => setPassword(e.target.value)} value={password} onKeyUp={getEnter}/>
+                        </label>
+                        {status === "error" ? <p className={style.login__content_errorLogin}>Erro no login...</p> : false}
+                        <button className={`${style.login__content__button_login} ${status === "loading" || status === "success" ? style.active: ""}`} onClick={() => mutate()}>Login</button>
+                    </div>
                 </div>
             </div>
         </BackgroundDropdown>
